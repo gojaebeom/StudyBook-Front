9b6ec6908ce8b1c23d6778c9df27c0ab5f8d314c
gojaebeom <const.gjb@gmail.com>
2021-07-12 20:14:13 +0900

    update

--- src/components/widgets/Search.js
+++ src/components/widgets/Search.js
@@ -4,7 +4,13 @@ export default function Search() {
 
     return (
     <div className="search-container">
+        <div className="icon-container">
+
+        </div>
         <input placeholder="찾으시는 글이 있나요?"/>
+        <div className="button-container">
+            <button><i className="fas fa-search"></i></button>
+        </div>
     </div>
     );
 }

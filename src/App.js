72c4daa855ab9f8664c6bec08c9e7b9617c953a1
gojaebeom <const.gjb@gmail.com>
2021-07-12 20:28:52 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from "react";
 import { Route, Switch } from "react-router-dom";
-import JoinForm from "./components/widgets/JoinForm";
-import LoginForm from "./components/widgets/LoginForm";
-import DefaultLayout from "./components/layouts/DefaultLayout";
-import SignLayout from "./components/layouts/SignLayout";
-import Feed from "./components/containers/Feed";
+import Feed from "./components/pages/Feed";
 
 export default function App() {
     return (
-    <div className="app-container">
+    <div className="w-full flex-col justify-center items-center">
+        <header className="w-full border p-5">
+            <figure className="border">StudyBook</figure>
+        </header>
         <Switch>
             <Route exact path="/">
-                <DefaultLayout>
-                    <Feed />
-                </DefaultLayout>
+                <Feed />
             </Route>
             <Route exact path="/login">
-                <SignLayout children={<LoginForm />}/>
+
             </Route>
             <Route exact path="/join">
-                <SignLayout children={<JoinForm />}/>
+
             </Route>
         </Switch>
     </div>
--- src/components/layouts/SignLayout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SignLayout(props){
-    return (
-    <div className="sign-container">
-        { props.children }
-    </div>
-    );
-} 
